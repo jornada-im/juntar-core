f56a315f0ff06d3eaa1d49c878c23b90a59212a7
Gregory Maurer <gregmaurer@gmail.com>
2026-06-08 15:18:57 -0600

    More validation of new plant list, preparing for publication

--- build_dataset/jrn520_taxa/jrn520001_taxa_plants.R
+++ build_dataset/jrn520_taxa/jrn520001_taxa_plants.R
@@ -5,8 +5,10 @@ library(tidyverse)
 library(readxl)
 
 source('config.R')
-out_path <- paste(im_path, 'Core_packages/210520001_taxa_plants', sep='/')
-in_path <- paste(im_path, "dataprep", "jrn520_taxa", "plant_list", sep="/")
+out_path <- file.path(im_path, "NonCore_packages", "210520001_taxa_plants")
+# Path to the plant list files (jrn520)
+in_path <- file.path(im_path, "dataprep", "jrn520_taxa", "plants")
+
 
 # Output data file 1 name
 f_out <- "jrn520001_JornadaPlantList.csv"
@@ -15,43 +17,55 @@ f_out <- "jrn520001_JornadaPlantList.csv"
 # John Anderson's list and one from Justin Van Zee.
 # John's original format (Plntalfa._20220303.txt) was pretty tough to parse so it
 # has been converted to an Excel sheet
-f_in1 <- "JRN_plant_species_list_main.xlsx"
+df_in <- read_excel(file.path(plants_path, "jrn_plant_list_MAIN.xlsx"), sheet='plant_list',
+                        skip=4, na = c(".", "NA"))
 
-# Load data.
-df_in <- read_xlsx(paste0(in_path, "/", f_in1), skip=4, na=c('.', '', ' ', 'NA'))
-names(df_in) <- tolower(names(df_in))
-
-# Add a binomial name column
-df <- df_in %>% mutate(
-  bin_usda = paste(genus_usda, species_usda, sep=' '),
-  bin_lter = paste(genus_lter, species_lter, sep=' ')
-)
-sapply(df, function(x) sum(is.na(x)))
-
+# Add a column for ITIS ids from taxadb
 library(taxadb)
-df['itis_id'] <- get_ids(df$bin_usda)
-df['itis_id2'] <- get_ids(df$bin_lter)
+df_in['itis_id'] <- get_ids(df_in$sciname)
 
-df[is.na(df$itis_id),]
+# About 25 taxa don't resolve at ITIS
+df_in[is.na(df_in$itis_id),]
+
 # Echinochloa crus-galli doesn't resolve to ITIS - should be ITIS:502210
 # Might be able to use get_names here - there are synonyms...
-df[df$bin_usda=='Echinochloa crus-galli', 'itis_id'] <- 'ITIS:502210'
+# df[df$bin_usda=='Echinochloa crus-galli', 'itis_id'] <- 'ITIS:502210'
 
-# Check some correspondence between 
-sum(df[df$bin_lter != 'NA NA','bin_usda']==df[df$bin_lter != 'NA NA','bin_lter'])
-# Why is this different? Maybe the 3 non-plants?
-sum(df$bin_lter==df$bin_usda)
-
-sum(df$bin_lter==df$bin_usda)
 df.export <- df_in 
 
 # Check for NAs and unique values of catvars
 sapply(df.export, function(x) sum(is.na(x)))
-unique(df.export$date)
-unique(df.export$plot)
-unique(df.export$ppttrt)
-unique(df.export$Ntrt)
+unique(df.export$family)
+unique(df.export$habit)
+unique(df.export$form)
+unique(df.export$cpath)
+unique(df.export$nativity)
+unique(df.export$habitat)
+unique(df.export$phenology)
+unique(df.export$reproduction)
+unique(df.export$lter_observed)
+unique(df.export$sciname_auth_follows)
+unique(df.export$usda_code_is_syn)
+
+# Export df.export as a csv
+options(scipen=999)   # turns of scientific notation
+write.csv(df.export, file.path(out_path, f_out), quote=F, row.names=F)
+
+# Output data file 2 name
+f_out <- "jrn520001_JornadaPlantList_synonyms.csv"
+
+# Read the synonyms file
+df_in <- read_excel(file.path(plants_path, "jrn_plant_list_MAIN.xlsx"), sheet='plant_synonyms',
+                        skip=4, na = c(".", "NA"))
+
+# 
+df.export <- df_in 
+
+# Check for NAs and unique values of catvars
+sapply(df.export, function(x) sum(is.na(x)))
+#unique(df.export$...)
+
 
 # Export df.export as a csv to current directory
 options(scipen=999)   # turns of scientific notation
-write.csv(df.export, f_out, quote=F, row.names=F)
+write.csv(df.export, file.path(out_path, f_out), quote=F, row.names=F)
